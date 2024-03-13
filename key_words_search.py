766f4dcd3bac070cea42634430cf55561ba2167b
Volodymyr Maryniak <volodymyr.maryniak@dataart.com>
2024-03-13 18:22:52 +0200

    Refactoring + multiprocessing approach

---
+++ key_words_search.py
@@ -0,0 +1,68 @@
+from concurrent.futures import Executor, as_completed
+from typing import Callable
+
+
+def concurrent_key_words_search(
+    file_paths: list[str],
+    key_words: list[str],
+    workers_count: int,
+    executor_factory: Callable[[int], Executor],
+) -> dict[str, list[str]]:
+
+    if len(file_paths) == 0:
+        raise ValueError("file_paths can't be empty")
+
+    if len(key_words) == 0:
+        raise ValueError("key_words can't be empty")
+
+    if workers_count <= 0:
+        raise ValueError("workers_count can't be less than or equal to 0")
+
+    # If the number of workers is greater than the number of files, then we will use the number of files as the number of threads
+    workers_count = min(workers_count, len(file_paths))
+
+    # Divide the file paths into groups
+    group_size = len(file_paths) // workers_count
+    file_paths_groups = [file_paths[i::group_size] for i in range(group_size)]
+
+    # Create an executor (ThreadPoolExecutor, ProcessPoolExecutor, etc.)
+    with executor_factory(workers_count) as executor:
+        return _execute_key_words_search(executor, file_paths_groups, key_words)
+
+
+def _execute_key_words_search(
+    executor: Executor, file_paths_groups: list[list[str]], key_words: list[str]
+):
+    results = {key_word: [] for key_word in key_words}
+
+    # Submit the tasks
+    futures = {
+        executor.submit(_search_key_words_in_files, file_paths_group, key_words)
+        for file_paths_group in file_paths_groups
+    }
+
+    # Get the results
+    for future in as_completed(futures):
+        result = future.result()
+        for key_word in key_words:
+            results[key_word].extend(result[key_word])
+
+    return results
+
+
+def _search_key_words_in_files(
+    file_paths: list[str], key_words: list[str]
+) -> dict[str, list[str]]:
+    results = {key_word: set() for key_word in key_words}
+    for file_path in file_paths:
+        try:
+            with open(file_path, "r") as file:
+                for line in file:
+                    for key_word in key_words:
+                        if key_word in line:
+                            results[key_word].add(file_path)
+                            break
+        except Exception as e:
+            print(f"An error occurred while processing the file {file_path}: {e}")
+
+    return results
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from pathlib import Path
 import time
-import threading_key_words_search
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, Executor
+from typing import Callable
 
-
-def get_file_paths(directory: str) -> list[str]:
-    directory_path = Path(directory)
-    txt_files = [str(file).replace("\\", "/") for file in directory_path.glob("*.txt")]
-    return txt_files
+from key_words_search import concurrent_key_words_search
 
 
 def main():
+    # Multithreading approach
+    test_approach(
+        "Multithreading", 3, lambda workers_count: ThreadPoolExecutor(workers_count)
+    )
 
+    # Multiprocessing approach
+    test_approach(
+        "Multiprocessing", 3, lambda workers_count: ProcessPoolExecutor(workers_count)
+    )
+
+
+def test_approach(
+    approach_name: str, workers_count: int, executor_factory: Callable[[int], Executor]
+):
     file_paths = get_file_paths("text_files")
 
     key_words = [
@@ -24,25 +34,14 @@ def main():
     ]
 
     start_time = time.time()
-    result = threading_key_words_search.search_key_words(file_paths, key_words, 3)
-    end_time = time.time()
-
-    execution_time_ms = (end_time - start_time) * 1000
-    print(
-        f"\nMultithreading approach execution time: {execution_time_ms} milliseconds\n"
+    result = concurrent_key_words_search(
+        file_paths, key_words, workers_count, executor_factory
     )
-    display_results(result)
-
-    start_time = time.time()
-    # TODO: Update it with multiprocessing approach
-    result = threading_key_words_search.search_key_words(
-        file_paths, key_words, 3
-    ) 
     end_time = time.time()
 
     execution_time_ms = (end_time - start_time) * 1000
     print(
-        f"\nMultiprocessing approach execution time: {execution_time_ms} milliseconds\n"
+        f"\n{approach_name} approach execution time: {execution_time_ms} milliseconds"
     )
     display_results(result)
 
@@ -51,6 +50,12 @@ def display_results(result: dict[str, list[str]]):
     for key_word, files in result.items():
         if files:
             print(f'"{key_word}": {files}')
+
+
+def get_file_paths(directory: str) -> list[str]:
+    directory_path = Path(directory)
+    txt_files = [str(file).replace("\\", "/") for file in directory_path.glob("*.txt")]
+    return txt_files
 
 
 if __name__ == "__main__":
